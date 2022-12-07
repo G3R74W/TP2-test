a23100abf0710374875099fba9db97c64d9f201d
geryaw <83832635+G3R74W@users.noreply.github.com>
2022-12-07 13:36:28 +0000

    some modification on the reservation class

--- TP2/Reservation.cpp
+++ TP2/Reservation.cpp
@@ -1,38 +1,35 @@
 #include "Reservation.h"
+#include "Date.h"
 
-Reservation::Reservation(Date start, int nightNbr, Hotel htl, Client touriste, Bedroom bdroom):_start(start), _nightNbr(nightNbr), _htl(htl), _touriste(touriste), _bdroom(bdroom)
-{
-  //works out the total price of the reservation
+Reservation::Reservation(Date start, int nightNbr, Hotel htl, Client touriste,
+                         Bedroom bdroom)
+    : _start(start), _nightNbr(nightNbr), _htl(htl), _touriste(touriste),
+      _bdroom(bdroom) {
+  // works out the total price of the reservation
   int price = _bdroom.getPrice();
   _totalPrice = _nightNbr * price;
 }
 
+// getters
 
-//getters
+// returns the date of the start of the reservation
+Date Reservation::getStartDate() { return _start; }
 
-//returns the date of the start of the reservation
-Date Reservation::getStartDate(){
-  return _start;
-}
+// returns the numbers of nights the clients wants to stay at the hotel
+int Reservation::getNightNbr() { return _nightNbr; }
 
-//returns the numbers of nights the clients wants to stay at the hotel
-int Reservation::getNightNbr(){
-  return _nightNbr;
-}
+// returns the total price of the reservation
+float Reservation::getTotalPrice() { return _totalPrice; }
 
-//returns the total price of the reservation
-float Reservation::getTotalPrice(){
-  return _totalPrice;
-}
+// setters
 
-//setters
+// changes the number of nights the clients wants to stay
+void Reservation::chngeNightNbr(int nightNbr) { _nightNbr = nightNbr; }
 
-//changes the number of nights the clients wants to stay 
-void Reservation::chngeNightNbr(int nightNbr){
-  _nightNbr = nightNbr;
-}
+// changes the date of the biginning of the reservation
+void Reservation::setNewDate(Date newStart) { _start = newStart; }
 
-//changes the date of the biginning of the reservation
-void Reservation::setNewDate(Date newStart){
-  _start = newStart;
+void Reservation::price() {
+  int price = _bdroom.getPrice();
+  _totalPrice = _nightNbr * price;
 }
--- TP2/Reservation.h
+++ TP2/Reservation.h
@@ -1,45 +1,45 @@
 #pragma once
-#include <iostream>
-#include <string>
-#include "Date.h"
 #include "Bedroom.h"
 #include "Client.h"
+#include "Date.h"
 #include "Hotel.h"
+#include <iostream>
+#include <string>
 
-class Reservation{
+class Reservation {
 private:
-  //date of the begining of the reservation
+  // date of the begining of the reservation
   Date _start;
-  //Number of nights spent at the hotel
+  // Number of nights spent at the hotel
   int _nightNbr;
-  //total price of the reservation
+  // total price of the reservation
   float _totalPrice;
 
-  //bedroom - Hotel & client objects
+  // bedroom - Hotel & client objects
   Bedroom _bdroom;
   Hotel _htl;
   Client _touriste;
-  
+
 public:
-  Reservation(Date start, int nightNbr, Hotel htl, Client touriste, Bedroom bdroom);
+  Reservation(Date start, int nightNbr, Hotel htl, Client touriste,
+              Bedroom bdroom);
+  void price();
+  // getters
 
-//getters
+  // returns the date of the start of the reservation
+  Date getStartDate();
 
-//returns the date of the start of the reservation
-Date getStartDate();
+  // returns the numbers of nights the clients wants to stay at the hotel
+  int getNightNbr();
 
-//returns the numbers of nights the clients wants to stay at the hotel
-int getNightNbr();
+  // returns the total price of the reservation
+  float getTotalPrice();
 
-//returns the total price of the reservation
-float getTotalPrice();
+  // setters
 
-//setters
+  // changes the number of nights the clients wants to stay
+  void chngeNightNbr(int nightNbr);
 
-//changes the number of nights the clients wants to stay 
-void chngeNightNbr(int nightNbr);
-
-//changes the date of the biginning of the reservation
-void setNewDate(Date newStart);
-
+  // changes the date of the biginning of the reservation
+  void setNewDate(Date newStart);
 };
